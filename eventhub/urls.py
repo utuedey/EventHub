81b6486a1326baaadb88b8b5718d7193e1d4ecd5
utuedey <utuedeyejosph@gmail.com>
2023-09-19 11:54:07 +0100

    include core urls

--- eventhub/urls.py
+++ eventhub/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('apps.users.urls')),
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
 ]
 
