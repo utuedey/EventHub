567fb9d5a13c944dd3e16edd033fa39c3024244b
Goldneo10 <ibrahimmuhammad271@gmail.com>
2023-09-28 10:29:10 +0100

    fixed url clash

--- core/urls.py
+++ core/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('events/<int:event_id>/unregister/', views.EventUnregistrationView.as_view(), name='event-unregistration'),
     path('search/', views.EventSearchView.as_view(), name='event-search'),
     path('categories/<int:pk>/', views.EventCategoryView.as_view(), name='event-category'),
-    path('api/categories/', views.AllCategoriesView.as_view(), name='all-categories'),
+    path('categories/', views.AllCategoriesView.as_view(), name='all-categories'),
     path('tags/<int:pk>/', views.EventTagView.as_view(), name='event-tag'),
 ]
--- eventhub/urls.py
+++ eventhub/urls.py
@@ -20,8 +20,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('apps.users.urls')),
-    path('', include('core.urls')),
+    path('auth/', include('apps.users.urls')),
+    path('api/', include('core.urls')),
     path('admin/', admin.site.urls),
 ]
 
